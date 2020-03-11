139910e661316ae6c67c8b3fb5c9b8bf82e3e645
Sakchhi Srivastava <sakchhi.sri@gmail.com>
2020-03-11 22:50:46 +0530

    ADT - Definitions - Date ADT - Bag ADT

--- 1_ADT/bag.py
+++ 1_ADT/bag.py
@@ -9,6 +9,23 @@ remove(item) -- Remove and return item, raise exception if item not found
 iterator -- Creates and returns iterator to iterate over collection of items
 """
 
+from date import Date
+class _BagIterator:
+    def __init__(self, the_list):
+        self._bag_items = the_list
+        self._cur_item = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._cur_item < len(self._bag_items):
+            item = self._bag_items[self._cur_item]
+            self._cur_item += 1
+            return item
+        else:
+            raise StopIteration
+
 class Bag:
     def __init__(self):
         self.__items = list()
@@ -26,20 +43,19 @@ class Bag:
         assert item in self.__items, "The item must be in the bag"
         return self.__items.pop(self.__items.index(item))
 
-    def __iter__(self, item):
-        pass
+    def __iter__(self):
+        return _BagIterator(self.__items)
+
 
 
 if __name__ == '__main__':
     my_bag = Bag()
-    my_bag.add(19)
-    my_bag.add(74)
-    my_bag.add(23)
-    my_bag.add(19)
-    my_bag.add(12)
+    my_bag.add(Date(6,28,1991))
+    my_bag.add(Date(2,27,1994))
+    # my_bag.add("23")
+    # my_bag.add("19")
+    # my_bag.add("12")
 
-    value = int(input("Guess a value contained in the bag: "))
-    if value in my_bag:
-        print("Bag contains the value", value)
-    else:
-        print("The bag does not contain the value", value)
+    value = input("Guess a value contained in the bag: ")
+    for value in my_bag:
+        print(value)
--- 1_ADT/checkdates2.py
+++ 1_ADT/checkdates2.py
@@ -3,14 +3,18 @@ from date import Date
 
 def main():
     born_before = Date(6, 1, 1988)
+    print(born_before)
     bag = Bag()
 
     date = prompt_and_extract_date()
     while date is not None:
         bag.add(date)
+        print(date)
         date = prompt_and_extract_date()
 
+    print(len(bag))
     for date in bag:
+        print(date)
         if date <= born_before:
             print("Should be atleast 21 years of age", date)
 
@@ -24,3 +28,5 @@ def prompt_and_extract_date():
         day = int(input("day: "))
         year = int(input("year: "))
         return Date(month, day, year)
+
+main()
